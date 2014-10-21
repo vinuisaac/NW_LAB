a5e5b5b06e76d5adc5bed8efe8c5077bb77a0658
Vinu Isaac <vinuisaacj@outlook.com>
2014-10-21 15:55:07 +0530

    Update p3.tcl

--- lab3/p3.tcl
+++ lab3/p3.tcl
@@ -4,70 +4,50 @@ $ns color 1 Blue
 $ns color 2 Red
 
 
-set ntrace [open prg3.tr w]
-$ns trace-all $ntrace
+set tracefile [open p3.tr w]
+$ns trace-all $tracefile
 
-set namfile [open prg3.nam w]
+set namfile [open p3.nam w]
 $ns namtrace-all $namfile
 
 
-proc finish { } {
+proc finish {} {
 		global ns ntrace namfile
 	
-			$ns flush-trace
-			close $ntrace
-			close $namfile
-
-		exec nam prg3.nam &
-		
+		$ns flush-trace
+		close $tracefile
+		close $namfile
+		exec nam p3.nam &
 		exit 0
 	}
 
-
-    for { set i 0 } {$i < 7 } { incr i } {
-		set n($i) [$ns node]
-	}
-
-
-      $n(0) label "Udp Source "
-      $n(6) label "Udp Sink"
-
-      			
-
+	set n0 [$ns node]
+	set n1 [$ns node]
+	set n2 [$ns node]
+	set n3 [$ns node]
+	set n4 [$ns node]
+	set n5 [$ns node]
+	set n6 [$ns node]
+	
+set lan [$ns newLan "$n0 $n1 $n2 $n3 $n4 $n5 $n6" 0.5Mb 40ms LL Queue/DropTail MAC/802_3 Channel]
 
 
-	set lan [$ns newLan "$n(0) $n(1) $n(2) $n(3) $n(4) $n(5) $n(6)" 0.5Mb 40ms LL Queue/DropTail MAC/802_3 Channel]
+set udp [new Agent/UDP]
+$ns attach-agent $n0 $udp
 
+set null [new Agent/Null]
+$ns attach-agent $n6 $null
 
-    #   $ns make-lan -trace on "$n(0) $n(1) $n(2) $n(3) $n(4) $n(5)"  0.5Mb 40ms LL Queue/DropTail Mac/802_3 
-
-
-
-
- set udp [new Agent/UDP]
-$ns attach-agent $n(0) $udp
-
-set sink0 [new Agent/Null]
-$ns attach-agent $n(6) $sink0
-
-$ns connect $udp $sink0
+$ns connect $udp $null
 
 set cbr0 [new Application/Traffic/CBR]
 	$cbr0 set packetSize_  1000
 	$cbr0 set interval_ 0.1
 	$cbr0 attach-agent $udp
 
-
-
-$udp set fid_ 1
-
-
-
-
 $ns at 0.01 "$cbr0 start"
 $ns at 20.0 "$cbr0 stop"
 $ns at 25.0 "finish"
-
 
 $ns run 
  
